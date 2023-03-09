8532ea855ff469825f3227381471088cdf6cf476
eddysoniwu <iwuohaeddyson2400@gmail.com>
2023-03-09 18:17:01 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,69 @@
+let id = (id) => document.getElementById(id);
+
+let classes = (classes) => document.getElementsByClassName(classes);
+
+let username = id("username"),
+email = id("email")
+password = id("password")
+label = id("label")
+
+errorMsg = classes("error")
+successicon = classes("success-icon");
+failureicon = classes("failure-icon");
+
+label.addEventListener("submit",(e)=> {
+    e.preventDefault();
+    engine(username, 0, "username cannot be blank");
+    engine(email, 1, "Email cannot be blank");
+    engine(password, 2, "password cannot be blank")
+});
+
+let engine = (id, serial, message) => {
+    if(id.value.trim() === "") {
+        errorMsg[serial].innerHTML = message;
+        id.style.border = "3% solid red"
+        //icons
+        failureicon[serial].style.opacity = "1"
+    }
+    else{
+        errorMsg[serial].innerHTML = "";
+        id.style.border = "3% solid green";
+        //icons
+        successicon[serial].style.opacity = "1"
+        failureicon[serial].style.opacity = "0"        
+    }
+    console.log(serial)
+}
+
+let sideview = document.querySelector('.sideview')
+document.querySelector('.bars>i').addEventListener('click', (e) => {
+    if( !e.target.classList.contains('fa-times')) {
+        e.target.classList.add('fa-times')
+        e.target.classList.remove('fa-bars')
+        sideview.style.width = '20rem'
+    }else {
+        e.target.classList.remove('fa-times')
+        e.target.classList.add('fa-bars')
+        sideview.style.width = '0'
+    }
+})
+
+
+document.querySelector('.floatbtn').addEventListener('click', (e) => {
+    // alert('from floatbtn')
+    if(!e.target.classList.contains('secondfloat')) {
+        e.target.classList.add('secondfloat')
+        e.target.style.right = '14.5rem'
+        setTimeout(() => {
+            document.querySelector('.floatinput').style.width = '15.5rem'
+            document.querySelector('.floatinput').style.transition = 'width 600ms ease-in'
+        }, 700)
+    }else {
+        e.target.classList.remove('secondfloat')
+        document.querySelector('.floatinput').style.width = '0'
+        document.querySelector('.floatinput').style.transition = 'width 600ms ease-in'
+        setTimeout(() => {
+            e.target.style.right = '1rem'
+        }, 700)
+    }
+})
